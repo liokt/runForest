d51c9b71e4cf54bc3a3862ecc81ed148776f9edc
Lio <liomazza@gmail.com>
2021-04-04 16:16:09 -0300

    setup the barchart in statistics fragment

--- app/src/main/java/com/example/lio/runforest/ui/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/example/lio/runforest/ui/fragments/StatisticsFragment.kt
@@ -1,36 +1,62 @@
 package com.example.lio.runforest.ui.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.example.lio.runforest.R
+import com.example.lio.runforest.other.CustomMarkerView
 import com.example.lio.runforest.other.TrackingUtility
 import com.example.lio.runforest.ui.viewmodels.StatisticsViewModel
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_statistics.*
 import kotlin.math.round
 
 @AndroidEntryPoint
-class StatisticsFragment: Fragment(R.layout.fragment_statistics) {
+class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
 
     private val viewModel: StatisticsViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         subscribeToObservers()
+        setupBarChart()
 
+    }
+
+    private fun setupBarChart() {
+        barChart.xAxis.apply {
+            position = XAxis.XAxisPosition.BOTTOM
+            setDrawLabels(false)
+            axisLineColor = Color.WHITE
+            textColor = Color.WHITE
+            setDrawGridLines(false)
+        }
+        barChart.axisLeft.apply {
+            axisLineColor = Color.WHITE
+            textColor = Color.WHITE
+            setDrawGridLines(false)
+        }
+        barChart.apply {
+            description.text = "Avg speed Over Time"
+            legend.isEnabled = false
+        }
     }
 
     private fun subscribeToObservers() {
         viewModel.totalTimeRun.observe(viewLifecycleOwner, Observer {
-            it?.let{
-            val totalTimeRun = TrackingUtility.getFormattedStopWatchTime(it)
+            it?.let {
+                val totalTimeRun = TrackingUtility.getFormattedStopWatchTime(it)
                 tvTotalTime.text = totalTimeRun
-        }
+            }
         })
-
         viewModel.totalDistance.observe(viewLifecycleOwner, Observer {
             it?.let {
                 val km = it / 1000f
@@ -47,12 +73,22 @@ class StatisticsFragment: Fragment(R.layout.fragment_statistics) {
             }
         })
         viewModel.totalCaloriesBurned.observe(viewLifecycleOwner, Observer {
-            it?.let{
+            it?.let {
                 val totalCalories = "${it}kcal"
                 tvTotalCalories.text = totalCalories
             }
         })
+        viewModel.runsSortedByDate.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                val allAvgSpeeds = it.indices.map { i -> BarEntry(i.toFloat(), it[i].avgSpeedInKMH) }
+                val bardataSet = BarDataSet(allAvgSpeeds, "Avg Speed Over Time").apply {
+                    valueTextColor = Color.WHITE
+                    color = ContextCompat.getColor(requireContext(), R.color.colorAccent)
+                }
+                barChart.data = BarData(bardataSet)
+                barChart.marker = CustomMarkerView(it.reversed(), requireContext(), R.layout.marker_view)
+                barChart.invalidate()
+            }
+        })
     }
-
-
 }
